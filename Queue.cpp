c35aa045df7be5164f66cdb3f3621d5ead34bbe6
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2021-11-23 16:04:07 +0300

    Added comments

--- Queue.cpp
+++ Queue.cpp
@@ -3,39 +3,52 @@
 // Циклический список. Символьный. выборка из очереди элемента с заданным значением приоритета.
 #include <iostream>
 
-struct QItem {
-	char value;
-	int priority;
+// Структура представляет собой элемент очереди с приоритетом
+struct PQItem {
+	char value; // Значение очереди
+	int priority; // Приоритет в очереди
 };
 
-struct Queue {
-	QItem** values;					// queue container
-	int rear;                  // index of first element
-	int front;                   // index of last element
-	int count;          // number of elements currently in queue
-	int size; 
+// Структура представляет собой очередь с приоритеотм
+struct PQueue {
+	PQItem** values; // Значения очереди
+	int rear; // Индекс свободного места для вставки
+	int front; // Индекс старейшего (по порядку вставок) элемента
+	int count; // Количество элементов
+	int size;  // Размер очереди
 };
 
-void initQueue(Queue* queue, int size) {
+// Инициализирует созданную очередь
+// queue - указазатель на очередь
+// size = максимальный размер очереди
+void initPQueue(PQueue* queue, int size) {
 	queue->rear = 0;
-	queue->front = size - 1;
+	queue->front = 0;
 	queue->count = 0;
 	queue->size = size;
-	queue->values = new QItem*[size];
+	queue->values = new PQItem*[size]; //Создаём массив указателей
 
+	// Инициализируем массив нулями
 	for (size_t i = 0; i < size; i++)
 		queue->values[i] = NULL;
 }
 
-void printQueue(Queue* queue) {
+// Выводит элементы очереди на печать поочерёдно
+// queue - указазатель на очередь
+void printPQueue(PQueue* queue) {
+
+	// Если очередь не создана, то выводим ошибку
 	if (queue->size == 0) {
 		std::cout << "Queue is not created" << std::endl;
 		return;
 	}
 
+	// Выводим заголовок
 	std::cout << "\nQueue:\n";
+	// Проходимся по каждому элементу очереди
 	for (size_t i = 0; i < queue->size; i++)
 	{
+		// Проверяем на NULL, чтобы корректно отработать все возможные значения
 		if (queue->values[i] != NULL) {
 				std::cout << "[" << i << "] " << queue->values[i]->value << " : " << queue->values[i]->priority << std::endl;
 		}
@@ -45,24 +58,37 @@ void printQueue(Queue* queue) {
 	}
 	
 }
-
-void insertQueue(Queue* queue, char val, int priority ) {
+// Вставляет в очередь новый элемент
+// queue - указазатель на очередь
+// val - значение
+// priority - приоритет значения
+void insertIntoPQueue(PQueue* queue, char val, int priority ) {
+	// Если очередь не создана, то выводим ошибку
 	if (queue->size == 0) {
 		std::cout << "Queue is not created" << std::endl;
 		return;
 	}
+	// Если очередь уже заполнена, то выводим ошибку
 	if (queue->count == queue->size) {
 		std::cout << "Queue overflow!" << std::endl;
 	}
-	QItem* item = new QItem;
+	// Создаём новый элемент из данных
+	PQItem* item = new PQItem;
 	item->value = val;
 	item->priority = priority;
+
+	// Вставляем элемент в свободное место
 	queue->values[queue->rear] = item;
+	// Циклически сдвигаем индекс свободного места
 	queue->rear = (queue->rear + 1) % queue->size;
+	// Число элементов увеличивается на 1
 	queue->count++;
 }
 
-void clearQueue(Queue* queue) {
+// Удаляет очередь и освобождает занятые ей ресурсы
+// queue - указазатель на очередь
+void deletePQueue(PQueue* queue) {
+	// Проверяем, создана ли очередь. Иначе нет смысла её удалять
 	if (queue->size != 0) {
 		queue->rear = 0;
 		queue->front = 0;
@@ -72,26 +98,43 @@ void clearQueue(Queue* queue) {
 		delete queue->values;
 	}
 }
-
-QItem* getElementByPriority(Queue* queue, int priority) 
+// Выбирает из очереди элемент с заданным значением
+// queue - указазатель на очередь
+// priority - нужный приоритет
+// Возвращает указатель на элемент
+PQItem* getElementByPriority(PQueue* queue, int priority) 
 {
+	// Если в очереди нет элементов, то выводим ошибку
 	if (queue->count == 0) {
 		std::cout << "Empty queue!" << std::endl;
 		return NULL;
 	}
-
+	
+	// Проходим по каждому элементу очереди
 	for (size_t i = 0; i < queue->size; i++)
 	{
+		// Если у элемента нужный приоритет
 		if (queue->values[i]->priority == priority) {
-			QItem* val = queue->values[i];
+			// Запоминаем значения нужного элемента
+			PQItem* val = new PQItem;
+			val->value = queue->values[i]->value;
+			val->priority = queue->values[i]->priority;
+
+			// Меняем местами с front элементом
 			queue->values[i] = queue->values[queue->front];
+			// Устанавливаем front в NULL
 			queue->values[queue->front] = NULL;
+			// Число элементов уменьшается на 1
 			queue->count--;
+			
+			// Циклически сдвигаем front
 			queue->front = (queue->front + 1) % queue->size;
+
 			return val;
 		}
 	}
 	
+	// Элемент не найден. Возвращаем NULL
 	return NULL;
 
 }
@@ -99,8 +142,12 @@ QItem* getElementByPriority(Queue* queue, int priority)
 
 int main()
 {
+	// Инциализируем генератор случайных чисел
 	srand(124);
-	Queue* queue = new Queue;
+
+	// Создаём очередь
+	PQueue* queue = new PQueue;
+	// Устанавливаем размер в 0, чтобы пометить, что очередь не инициализирована
 	queue->size = 0;
 
 	while (true) {
@@ -122,10 +169,10 @@ int main()
 		case 1:
 			std::cout << "Size: ";
 			std::cin >> input;
-			initQueue(queue, input);
+			initPQueue(queue, input);
 			break;
 		case 2:
-			printQueue(queue);
+			printPQueue(queue);
 			break;
 		case 3:
 			std::cout << "Value: ";
@@ -135,10 +182,10 @@ int main()
 			std::cout << "Priority: ";
 			int priority;
 			std::cin >> priority;
-			insertQueue(queue, character, priority);
+			insertIntoPQueue(queue, character, priority);
 			break;
 		case 4:
-			clearQueue(queue);
+			deletePQueue(queue);
 			break;
 		case 5:
 			std::cout << "Priority: ";
@@ -150,7 +197,6 @@ int main()
 			else {
 				std::cout << "Value not found"  << std::endl;
 			}
-			
 			break;
 		}
 	}
