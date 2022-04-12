891234a960721993d045476c36b9d1da9253ede6
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2022-04-12 17:19:42 +0300

    Update summator.v

--- Verilog/LR2/v2/summator.v
+++ Verilog/LR2/v2/summator.v
@@ -1,18 +1,17 @@
-module summator #(parameter reglength = 3)(input wire[reglength-1:0] r1, r2, input wire clk, output wire sum);
+module summator #(parameter reglength = 3)(input wire[reglength-1:0] r1, r2, input wire clk, reset, output wire sum);
 	reg[reglength:0] accumulator = 0;
 
 	assign sum = accumulator[0];
 
-	reg init = 1;
 	reg[reglength-1:0] reg1, reg2 = 0;
 
+	always @ (reset) begin
+		reg1 = r1;
+		reg2 = r2;
+		accumulator = reg1 + reg2;
+	end
+
 	always @ (posedge clk) begin
-		if (init) begin
-			reg1 = r1;
-			reg2 = r2;
-			init = 0;
-			accumulator = reg1 + reg2;
-		end
 		accumulator = accumulator >> 1;
 	end
 endmodule
