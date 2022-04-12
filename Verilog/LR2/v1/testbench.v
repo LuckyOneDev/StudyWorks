b39a9b8cd8e800045cef4e9f7d4a3c9e8435a8d7
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2022-04-12 16:01:55 +0300

    Update testbench.v

--- Verilog/LR2/v1/testbench.v
+++ Verilog/LR2/v1/testbench.v
@@ -1,18 +1,18 @@
-module SumTB  #(parameter reglength = 3, r1 = 0, r2 = 0)();
+module SumTB  #(parameter reglength = 3, r1 = 7, r2 = 7)();
 	reg clk = 0;
 	
 	reg[reglength-1:0] reg1 = r1;
 	reg[reglength-1:0] reg2 = r2;
-	
-	wire[reglength:0] summa = 0;
-	
-	summator #(.reglength(3), .r1val(r1), .r2val(r2)) sm_inst(.r1(reg1), .r2(reg2), .clk(clk), .summa(summa));
+
+	summator #(.reglength(reglength)) sm_inst(.r1(reg1), .r2(reg2), .clk(clk), .sum(sum));
+
+	always @(posedge clk) begin
+		$display("sum: %b", sum);
+	end
 
 	initial
 		begin
-			#8
-			$display("r1: %d\nr2: %d summ: %d", r1, r2, summa);
-			$finish;
+			#12 $finish;
 		end
 
 	always 
