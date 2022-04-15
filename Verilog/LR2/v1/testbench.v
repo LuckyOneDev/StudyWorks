d2d84d0c1acd1bf5c23124a8da557b57e670f984
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-04-15 18:30:52 +0300

    Reworked testbench for v1.

--- Verilog/LR2/v1/testbench.v
+++ Verilog/LR2/v1/testbench.v
@@ -1,35 +1,52 @@
 module SumTB #(parameter reglength = 3);
 	reg clk = 0;
-	
-	reg[reglength:0] i, j = 0;
 
-	reg[reglength-1:0] reg1 = 0;
-	reg[reglength-1:0] reg2 = 0;
+	reg[reglength:0] i, j, k = 0;
 
-	summator #(.reglength(reglength)) sm_inst(.r1(reg1[0]), .r2(reg2[0]), .clk(clk), .sum(sum));
+	reg[reglength:0] sumReg;
+	reg[0:0] A;
+	reg[0:0] B;
+	reg[0:0] testErrorN;
+	wire[0:0] sum;
 
-	always @(posedge clk) begin
-		$display(sum);
-		reg1 = reg1 >> 1;
-		reg2 = reg2 >> 1;
-	end
+	summator #(.reglength(reglength)) sm_inst(.r1(A), .r2(B), .clk(clk), .sum(sum));
 
 	initial
 		begin
-			for (i = 0; i < 2 ** reglength; i = i + 1) begin
-				reg2 = 0;
-    			for (j = 0; j < 2 ** reglength; j = j + 1) begin
-					$display("%d + %d:", reg1, reg2);
-					#16
-					$display("end");
-					reg2 = j + 1;
-					reg1 = i;
-				end
-				reg1 = i + 1;
-			end
-			$finish;
-		end
+			clk <= 0;
+			sumReg <= 0;
+			@(posedge clk);
+			for (i = 1; i < 2 ** reglength; i = i + 1) begin
+				for (j = 1; j < 2 ** reglength; j = j + 1) begin
+					for (k = 0; k < reglength; k = k + 1) begin
+						A <= i[k];
+						B <= j[k];
+						@(posedge clk);
+					end
 
-	always 
+				A <= 0;
+				B <= 0;
+				@(posedge clk);
+
+				for (k = 0; k < reglength + 1; k = k + 1) begin
+						sumReg[k] <= sum;
+						@(posedge clk);
+					end
+
+				testErrorN <= sumReg == i + j ? 1 : 0;
+				@(posedge clk);
+
+				sumReg <= 0;
+				@(posedge clk);
+				end
+			end
+			$finish();
+		end
+	initial begin
+		$dumpfile("test.vcd");
+		$dumpvars(0,SumTB);
+	end
+	always
 		#1 clk = ~clk;
+
 endmodule
