c4ab3e2b2c313d308ff09f2ab1c7ab6930a19dc4
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-04-16 19:34:03 +0300

    Edited summator for synthesis support

--- Verilog/LR2/v1/summator.v
+++ Verilog/LR2/v1/summator.v
@@ -4,9 +4,9 @@ module summator #(parameter reglength = 3)(input wire r1, r2, input wire clk, ou
 	fullsum s(.A(reg1[0]), .B(reg2[0]), .clk(clk), .sum(sum));
 
 	always @ (posedge clk) begin
+		reg1 <= reg1 >> 1;
+		reg2 <= reg2 >> 1;
 		reg1[reglength-1] <= r1;
 		reg2[reglength-1] <= r2;
-		reg1 = reg1 >> 1;
-		reg2 = reg2 >> 1;
 	end
 endmodule
