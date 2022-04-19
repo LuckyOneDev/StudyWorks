7f874dc51fee319e4d62d3b1743e5bca60248083
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2022-04-19 15:52:37 +0300

    Update testbench.v

--- Verilog/LR2/v2/testbench.v
+++ Verilog/LR2/v2/testbench.v
@@ -1,26 +1,28 @@
 module SumTB #(parameter reglength = 3);
 	reg clk = 0;
 	
+	reg[reglength:0] i = 0;
+
 	reg[reglength-1:0] reg1 = 0;
 	reg[reglength-1:0] reg2 = 0;
 
 	reg reset = 0;
+	wire[reglength:0] sum;
 
-	summator #(.reglength(reglength)) sm_inst(.r1(reg1), .r2(reg2), .clk(clk), .sum(sum), .reset(reset));
+	summator #(.reglength(reglength)) sm_inst(.r1(reg1), .r2(reg2), .clk(clk), .sum(sum));
 
-	always @(posedge clk) begin
-		$display(sum);
-	end
+	always 
+		#1 clk = ~clk;
 
 	initial
 		begin
 			repeat (2 ** reglength) begin
 				reg2 = 0;
 				repeat (2 ** reglength) begin
-					$display("%d + %d:", reg1, reg2);
-					#8 
-					$display("end");
-					reset = ~reset;
+					$display("%d + %d =", reg1, reg2);
+					#4
+					$display(sum);
+					$display("\n");
 					reg2 = reg2 + 1;
 				end
 				reg1 = reg1 + 1;
@@ -28,6 +30,8 @@ module SumTB #(parameter reglength = 3);
 			$finish;
 		end
 
-	always 
-		#1 clk = ~clk;
+	initial begin
+		$dumpfile("var_dump.vcd");
+		$dumpvars;
+	end
 endmodule
