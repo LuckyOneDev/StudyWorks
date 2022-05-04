bc960cfddb355fc849beffe68ec3cea5b19207ab
Dmitry Parfenyuk <cekunda.rf@gmail.com>
2022-05-04 19:11:00 +0300

    Another hotfix

--- Verilog/LR3/ram.sv
+++ Verilog/LR3/ram.sv
@@ -8,11 +8,11 @@ module ram #(
     input logic [$clog2(word_amount):0] address,  // адрес обращения
     input logic select,  // признак обращения
     input logic operation,  // тип операции (запись/чтение)
-    input logic [word_size:0] wdata,  // данные для записи
-    output logic [word_size:0] rdata  // прочитанные данные
+    input logic [word_size-1:0] wdata,  // данные для записи
+    output logic [word_size-1:0] rdata  // прочитанные данные
 );
 
-  reg [word_size:0][word_amount:0] memory = 0;
+  reg [word_amount-1:0][word_size-1:0] memory = 0;
 
   always @(posedge select) begin
     if (operation == `READ) begin
