a377243a844bd49877bed5b12a10617d61bac29f
Ivan Mokshin <81585996+LuckyOneDev@users.noreply.github.com>
2022-05-15 18:30:12 +0300

    Update timer.sv

--- Verilog/Kursovaya/timer.sv
+++ Verilog/Kursovaya/timer.sv
@@ -1,13 +1,10 @@
-`define IDLE 0
-`define SETUP 1
-`define WRITE 2
-`define READ 3
-
-module timer #(
+module timer
+#(
+    parameter timerBaseAddr = 0,
     parameter timerbits = 8,
-    parameter addrWidth = 2,
-    parameter timerBaseAddr = 0
-) (
+    parameter addrWidth = 2
+)
+(
     input sel,
     enable,
     reset,
@@ -20,9 +17,18 @@ module timer #(
     slverr
 );
 
+  localparam STATE_IDLE = 0;
+  localparam STATE_SETUP = 1;
+  localparam STATE_WRITE = 2;
+  localparam STATE_READ = 3;
+
+  localparam CTR_STATUS_ADDR = timerBaseAddr;
+  localparam CTR_GOAL_ADDR = timerBaseAddr + 1;
+  localparam CTR_CURR_ADDR = timerBaseAddr + 2;
+
   localparam CTR_STATUS_START = 0;
   localparam CTR_STATUS_STOP = 1;
-  localparam CTR_STATUS_STATE = 2;  //bits 2 and 3;
+  localparam CTR_STATUS_STATE = 2;
   localparam CTR_STATE_LEN = 2;
 
   typedef enum logic [CTR_STATUS_STATE+CTR_STATE_LEN-1:CTR_STATUS_STATE] {
@@ -32,61 +38,72 @@ module timer #(
   } e_ctr_state;
 
   logic [1:0] apb_state;
-
-  logic [timerbits:0] prdata;
-  assign rdata = prdata;
-
-  logic pslverr;
-  assign slverr = pslverr;
-
-  logic pready;
-  assign ready = pready;
-
-  logic [timerbits:0] max_value;
+  
   logic [timerbits:0] current_value;
-  logic is_running;
+  logic [timerbits:0] max_value;
+
+  logic [1:0] state_bits;
+
   e_ctr_state state;
+
   // Reset is zero positive
   always @(negedge reset) begin
-    apb_state = `IDLE;
+    apb_state <= STATE_IDLE;
   end
 
   always @(posedge clk) begin
-    if (is_running && current_value < max_value) current_value++;
+    if (state_bits[CTR_STATUS_START] && !state_bits[CTR_STATUS_STOP] && (current_value < max_value) ) current_value++;
 
-    if (addr >= timerBaseAddr && addr <= timerBaseAddr + addrWidth) begin
+    if (addr >= timerBaseAddr && addr <= timerBaseAddr + CTR_STATE_LEN) begin
       case (apb_state)
-        `IDLE: begin
-          pready  <= 1;
-          prdata  <= 0;
-          pslverr <= 0;
-          if (sel && !enable) apb_state = `SETUP;
+        STATE_IDLE: begin
+          rdata  <= 0;
+          slverr <= 0;
+          state_bits <= 0;
+        if (sel && !enable) apb_state = STATE_SETUP;
         end
-        `SETUP: begin
-          if (sel && enable && ready) begin
-            if (write) apb_state = `WRITE;
-            else apb_state = `READ;
+        STATE_SETUP: begin
+          if (sel && enable) begin
+            ready <= 1;
+            if (write) apb_state = STATE_WRITE;
+            else apb_state = STATE_READ;
           end
         end
-        `WRITE: begin
+        // WRITE
+        STATE_WRITE: begin
           if (sel && enable && write) begin
             case (addr)
-              0: begin
-                max_value = rdata;
+              CTR_STATUS_ADDR: begin
+                state_bits <= rdata;
               end
-              1: begin
-                is_running = rdata[0];
+              CTR_GOAL_ADDR: begin
+                max_value <= rdata;
+              end
+              CTR_CURR_ADDR: begin
+                slverr <= 1;
+                //current_value <= rdata;
               end
             endcase
-          end else apb_state = `IDLE;
+          end else apb_state = STATE_IDLE;
         end
-        `READ: begin
+        // READ
+        STATE_READ: begin
           if (sel && enable && !write) begin
-
-          end else apb_state = `IDLE;
+            case (addr)
+              CTR_STATUS_ADDR: begin
+                rdata <= state_bits;
+              end
+              CTR_GOAL_ADDR: begin
+                rdata <= max_value;
+              end
+              CTR_CURR_ADDR: begin
+                rdata <= current_value;
+              end
+            endcase
+          end else apb_state = STATE_IDLE;
         end
         default: begin
-          apb_state = `IDLE;
+          apb_state = STATE_IDLE;
         end
       endcase
     end
